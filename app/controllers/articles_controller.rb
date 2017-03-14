e9b5107b51a5b55ad4d9245af2878c65749daf7c
ignaciowg93 <ignaciowg93@gmail.com>
2017-03-14 17:16:19 -0300

    Agregando comentarios de noticia

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,46 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :edit , :update, :destroy]
+    def index
+      @articles = Article.all.order("created_at DESC")
+    end
+    def new
+       @article = Article.new
+    end
+    def show
+    end
+
+    def edit
+
+    end
+
+    def update
+      if @article.update(article_params)
+        redirect_to article_path(@article)
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+
+    end
+    def create
+      @article = Article.new(article_params)
+
+      if @article.save
+        redirect_to root_path
+      else
+        render 'new'
+      end  
+    end
+
+    private
+        def article_params
+          params.require(:article).permit(:headline,:lead,:body)
+        end
+
+        def find_article
+          @article = Article.find(params[:id])
+        end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
-
+  resources :articles do
+    resources :comments
+  end
+  root 'articles#index'
 end
